e8260778d01c58e99293e7db34e2ff1e5a90c996
ScarletLight <cameron.milton@gmail.com>
2019-04-14 14:21:55 +1000

    adding blank .gitignore

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Show me the money</title>
+		<link href="main.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php require_once('includes\header.php'); ?>
+<header>Hi</header>
+
+<?php require_once('includes\footer.php'); ?>
