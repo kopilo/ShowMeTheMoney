b5a9b1ad982dd38bf485061279e42759446c330c
ScarletLight <cameron.milton@gmail.com>
2019-04-22 15:12:37 +1000

    fixed 500 error

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once('includes\header.php'); ?>
+<?php include('includes\header.php'); ?>
 <header>Hi</header>
 
-<?php require_once('includes\footer.php'); ?>
+<?php include('includes\footer.php'); ?>
